57b830456d39d1844de98331046d4db8cef39210
Angelo Monte Serrat Bonini <abonini2000@gmail.com>
2024-04-27 15:54:13 -0300

    feat: invoicing forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pada-money';
+  invoiceForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.invoiceForm = this.formBuilder.group({
+      companyName: ['', Validators.required],
+      companyAddress: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipCode: ['', [Validators.required, Validators.pattern('^[0-9]{5}(?:-[0-9]{3})?$')]],
+      invoicingCompany: ['', Validators.required],
+      invoicingCompanyEmail: ['', [Validators.required, Validators.email]],
+      amount: ['', Validators.required],
+      currency: ['', Validators.required]
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.invoiceForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.invoiceForm.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.invoiceForm.value, null, 4));
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.invoiceForm.reset();
+  }
 }
